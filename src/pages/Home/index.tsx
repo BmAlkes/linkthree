1ee38b7c697c16787fe4dc7b89c9c4659baf3eff
bmalkes <bmalkes@gmail.com>
2023-11-01 13:12:41 +0200

    feat:Add home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,18 @@
 export const Home = () => {
   return (
-    <div className="w-full bg-neutral-950 h-screen">
-      <h1>pagina home</h1>
+    <div className="flex flex-col w-full py-4 items-center justify-center">
+      <h1 className="md:text-4xl text-3xl text-white mt-20 font-bold">
+        pagina home
+      </h1>
+      <span className=" text-gray-50 mb-5 mt-3"> See my Links 👇</span>
+      <main className="flex flex-col w-11/12 max-w-xl items-center text-center">
+        <section className="bg-white mb-4 w-full py-2 rounded-xl select-none transition-transform hover:scale-105 cursor-pointer">
+          <a href="">
+            <p className="text-base md:text-lg ">Canal no youtube</p>
+          </a>
+        </section>
+      </main>
+      <footer className="flex justify-center gap-3 my-4"></footer>
     </div>
   );
 };
